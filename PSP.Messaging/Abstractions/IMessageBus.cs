2f9b990c7f8362ffe8528cc764eeeff6dc0920f1
Saeed Ghafori <104858678+SaeedGhfori@users.noreply.github.com>
2026-07-21 02:24:43 +0330

    feat(messaging): add masstransit rabbitmq infrastructure

---
+++ PSP.Messaging/Abstractions/IMessageBus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSP.Abstractions
+{
+    internal class IMessageBus
+    {
+    }
+}
---
+++ PSP.Messaging/Consumers/IIntegrationEvent.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSP.Consumers
+{
+    internal class IIntegrationEvent
+    {
+    }
+}
