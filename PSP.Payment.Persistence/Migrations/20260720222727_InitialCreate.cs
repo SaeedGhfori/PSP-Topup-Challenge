fcab39eea82b484f6256915b22f934bd5d63d290
Saeed Ghafori <104858678+SaeedGhfori@users.noreply.github.com>
2026-07-21 01:58:41 +0330

    feat(payment): add ef core persistence and initial migration

---
+++ PSP.Payment.Persistence/Migrations/20260720222727_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PSP.Payment.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Pan = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    OperatorId = table.Column<int>(type: "int", nullable: false),
+                    TraceNumber = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    TerminalId = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Rrn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IdempotencyKey = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentTransactions", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentTransactions_IdempotencyKey",
+                table: "PaymentTransactions",
+                column: "IdempotencyKey",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentTransactions");
+        }
+    }
+}
