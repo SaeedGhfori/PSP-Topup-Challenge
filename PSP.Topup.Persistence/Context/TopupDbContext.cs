aa006376d424630716bb38938a72f0bf9dc81bcf
Saeed Ghafori <104858678+SaeedGhfori@users.noreply.github.com>
2026-07-20 10:35:55 +0330

    feat(persistence): add EF Core DbContext

---
+++ PSP.Topup.Persistence/Context/TopupDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+using PSP.Topup.Domain.Entities;
+
+namespace PSP.Topup.Persistence.Context;
+
+public sealed class TopupDbContext : DbContext
+{
+    public TopupDbContext(DbContextOptions<TopupDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TopupTransaction> TopupTransactions => Set<TopupTransaction>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TopupDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
