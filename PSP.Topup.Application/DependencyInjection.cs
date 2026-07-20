e70ae98198b0f230c39b741f517abd7443dea7e3
Saeed Ghafori <104858678+SaeedGhfori@users.noreply.github.com>
2026-07-20 19:26:48 +0330

    feat(validation): add fluent validation pipeline behavior

--- PSP.Topup.Application/DependencyInjection.cs
+++ PSP.Topup.Application/DependencyInjection.cs
@@ -1,6 +1,10 @@
 using FluentValidation;
 
+using MediatR;
+
 using Microsoft.Extensions.DependencyInjection;
+
+using PSP.Topup.Application.Behaviors;
 
 namespace PSP.Topup.Application;
 
@@ -13,6 +17,10 @@ public static class DependencyInjection
             cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
 
         services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+        services.AddTransient(
+             typeof(IPipelineBehavior<,>),
+             typeof(ValidationBehavior<,>));
 
         return services;
     }
